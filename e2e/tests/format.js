21c1ce0762365db1d311ba14898c698580915cb4
Matt Bierner <matb@microsoft.com>
2018-06-29 16:53:38 -0700

    Removing duplicate format setup code

--- e2e/tests/format.js
+++ e2e/tests/format.js
@@ -8,21 +8,9 @@ const mockFileName = path.join(__dirname, '..', 'project-fixture', 'main.ts');
 
 describe('Format', () => {
     it('should insert spaces between attributes names', () => {
-        const server = createServer();
-        openMockFile(server, mockFileName, 'const q = html`<span a="x"b="y"/>`\n');
-        server.send({
-            command: 'format',
-            arguments: {
-                file: mockFileName,
-                line: 1,
-                offset: 1,
-                endLine: 2,
-                endOffset: 1
-            }
-        });
-
-        return server.close().then(() => {
-            const response = getFirstResponseOfType('format', server);
+        return formatMockFile(
+            'const q = html`<span a="x"b="y"/>`\n'
+        ).then(response => {
             assert.isTrue(response.success);
             assert.strictEqual(response.body.length, 1);
             assert.strictEqual(response.body[0].newText, '<span a="x" b="y" />');
@@ -30,21 +18,9 @@ describe('Format', () => {
     });
 
     it('should not remove leading whitespace', () => {
-        const server = createServer();
-        openMockFile(server, mockFileName, 'html`\n<span />`\n');
-        server.send({
-            command: 'format',
-            arguments: {
-                file: mockFileName,
-                line: 1,
-                offset: 1,
-                endLine: 3,
-                endOffset: 1
-            }
-        });
-
-        return server.close().then(() => {
-            const response = getFirstResponseOfType('format', server);
+        return formatMockFile(
+            'html`\n<span />`\n'
+        ).then(response => {
             assert.isTrue(response.success);
             assert.strictEqual(response.body.length, 1);
             assert.strictEqual(response.body[0].newText, '<span />');
@@ -52,21 +28,9 @@ describe('Format', () => {
     });
 
     it('should not touch placeholders', () => {
-        const server = createServer();
-        openMockFile(server, mockFileName, 'const q = html`<span a="${123}">${123}</span>`\n');
-        server.send({
-            command: 'format',
-            arguments: {
-                file: mockFileName,
-                line: 1,
-                offset: 1,
-                endLine: 99,
-                endOffset: 1
-            }
-        });
-
-        return server.close().then(() => {
-            const response = getFirstResponseOfType('format', server);
+        return formatMockFile(
+            'const q = html`<span a="${123}">${123}</span>`\n'
+        ).then(response => {
             assert.isTrue(response.success);
             assert.strictEqual(response.body.length, 1);
             assert.strictEqual(response.body[0].newText, '<span a="${123}">${123}</span>');
@@ -74,85 +38,67 @@ describe('Format', () => {
     });
 
     it('should observe document indent rules', () => {
-        const server = createServer();
-        openMockFile(server, mockFileName, [
-            'html\`',
-            '<div>',
-            '<img/>',
-            '</div>`',
-        ].join('\n'));
-        server.send({
-            command: 'configure',
-            arguments: {
-                file: mockFileName,
-                formatOptions: {
-                    tabSize: 2,
-                    indentSize: 2,
-                    convertTabsToSpaces: true,
-                    newLineCharacter: "\n",
-                },
-            }
-        });
-        server.send({
-            command: 'format',
-            arguments: {
-                file: mockFileName,
-                line: 1,
-                offset: 1,
-                endLine: 99,
-                endOffset: 1
-            }
-        });
-
-        return server.close().then(() => {
-            const response = getFirstResponseOfType('format', server);
-            assert.isTrue(response.success);
-            assert.strictEqual(response.body.length, 1);
-            assert.strictEqual(response.body[0].newText, '<div>\n  <img/>\n</div>');
-        });
+        return formatMockFile(
+            [
+                'html\`',
+                '<div>',
+                '<img/>',
+                '</div>`',
+            ].join('\n'),
+            {
+                tabSize: 2,
+                indentSize: 2,
+                convertTabsToSpaces: true,
+                newLineCharacter: "\n",
+            })
+            .then(response => {
+                assert.isTrue(response.success);
+                assert.strictEqual(response.body.length, 1);
+                assert.strictEqual(response.body[0].newText, '<div>\n  <img/>\n</div>');
+            });
     });
 
     it('should not remove trailing newline', () => {
-        const server = createServer();
-        openMockFile(server, mockFileName, 'html`<span />\n`\n');
-        server.send({
-            command: 'format',
-            arguments: {
-                file: mockFileName,
-                line: 1,
-                offset: 1,
-                endLine: 99,
-                endOffset: 1
-            }
-        });
-
-        return server.close().then(() => {
-            const response = getFirstResponseOfType('format', server);
+        return formatMockFile(
+            'html`<span />\n`\n'
+        ).then(response => {
             assert.isTrue(response.success);
             assert.strictEqual(response.body.length, 1);
             assert.strictEqual(response.body[0].end.line, 1);
         });
     });
 
-
     it('should not remove newline if that is the only content', () => {
-        const server = createServer();
-        openMockFile(server, mockFileName, 'html`\n`\n');
-        server.send({
-            command: 'format',
-            arguments: {
-                file: mockFileName,
-                line: 1,
-                offset: 1,
-                endLine: 99,
-                endOffset: 1
-            }
-        });
-
-        return server.close().then(() => {
-            const response = getFirstResponseOfType('format', server);
+        return formatMockFile(
+            'html`\n`\n'
+        ).then(response => {
             assert.isTrue(response.success);
             assert.strictEqual(response.body.length, 0);
         });
     });
 })
+
+function formatMockFile(contents, options) {
+    const server = createServer();
+    openMockFile(server, mockFileName, contents);
+    if (options) {
+        server.send({
+            command: 'configure',
+            arguments: {
+                file: mockFileName,
+                formatOptions: options
+            }
+        });
+    }
+    server.sendCommand('format', {
+        file: mockFileName,
+        line: 1,
+        offset: 1,
+        endLine: 99,
+        endOffset: 1
+    });
+
+    return server.close().then(() => {
+        return getFirstResponseOfType('format', server);
+    });
+}
